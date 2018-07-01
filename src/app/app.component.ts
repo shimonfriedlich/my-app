fa7d127814e83e02bc047966b8d0167ebc745b77
shimonfriedlich <shimonfriedlich@gmail.com>
2018-07-01 21:23:34 +0300

    second time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  myObj = {
+    name:'avia',
+    anchor:'https://nrg.co.il'
+  }
+  clicked() {
+    console.log('hi');
+  }
+   
 }
+
